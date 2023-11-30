4b5aec9b8d95865b385f29258baaa56a6fbc22d7
radrien <raphael.adrien@daimler.com>
2023-11-30 18:05:55 +0000

    Add role security

--- auth/src/main/java/br/jus/tjms/auth/controller/Servidor.java
+++ auth/src/main/java/br/jus/tjms/auth/controller/Servidor.java
@@ -1,5 +1,6 @@
 package br.jus.tjms.auth.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,11 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class Servidor {
 
     @GetMapping("/")
+    @PreAuthorize("hasRole('USER')")
     public String listarServidores() {
         return "listar servidores";
     }
 
     @PostMapping("/")
+    @PreAuthorize("hasRole('ADMIN')")
     public String cadastrar() {
         return "cadastrar servidor";
     }
